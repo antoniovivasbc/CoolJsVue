b6011df3543442e9c9f50d04745d19deb62fcdb4
Antonio Vivas <antoniovivascavalcante@gmail.com>
2022-06-13 22:51:42 +0200

    First commit, Bootstrap instalation, projectComponent creation for the project list

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <projectComponent/>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import projectComponent from './components/projectComponent.vue'
+  
+  export default {
+    name: 'ProjectContent',
+    components: {
+      projectComponent
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
