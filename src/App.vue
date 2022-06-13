a7dc9e7584bfd534115479bfa3787402e2318ea8
Antonio Vivas <antoniovivascavalcante@gmail.com>
2022-06-13 22:56:06 +0200

    first commit, bootstrap 5 install

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,1 @@
-<template>
-  <projectComponent/>
-</template>
-<script>
-  import projectComponent from './components/projectComponent.vue'
-  
-  export default {
-    name: 'ProjectContent',
-    components: {
-      projectComponent
-    }
-  }
-</script>
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,2 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
-
-import { createApp } from "vue"
-import projects from './App.vue'
-
-createApp(projects).mount('#project-container')
